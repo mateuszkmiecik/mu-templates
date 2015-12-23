ef0110fa5d91019ccc871d7f628ea5e6e4e444ab
Mateusz Kmiecik <mk.chomik@gmail.com>
2015-12-23 22:59:42 +0100

    added translations

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
+var fs = require('fs');
 
 var mustache = require("gulp-mustache");
 var rename = require("gulp-rename");
 
 gulp.task('mustache', function() {
 
-return gulp.src("./templates/index.mustache")
-    .pipe(mustache())
-    .pipe(rename("index.html"))
-    .pipe(gulp.dest("./dist"));
+  var langs = fs.readdirSync('lang');
+  console.log(langs);
+
+
+  langs.forEach(function(l){
+    var prefix = l.split('.')[0]; // en, pl
+    gulp.src("./templates/index.mustache")
+        .pipe(mustache(require('./lang/'+l)))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest("./dist/"+prefix));
+  });
+
 
 });
 
